02a364296ab078ca506802510dbcc4ea9e6a7460
Utsav Barnwal <u.barnwal36@gmail.com>
2021-03-21 19:46:38 +0530

    add ListFilter

---
+++ custom/src/main/java/com/isolpro/custom/ListFilter.java
@@ -0,0 +1,48 @@
+package com.isolpro.custom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ListFilter<T> {
+
+  private final Callback<List<T>> onFilterChanged;
+  private List<T> items, filteredItems;
+
+  public ListFilter(Callback<List<T>> onFilterChanged) {
+    this.onFilterChanged = onFilterChanged;
+  }
+
+  public void setItems(List<T> items) {
+    this.items = items;
+
+    notifyFilterChanged();
+  }
+
+  protected abstract boolean compare(T item);
+
+  public void clearFilters() {
+    notifyFilterChanged();
+  }
+
+  private void filter() {
+    filteredItems = new ArrayList<>();
+
+    if (items == null) return;
+
+    for (T item : items) {
+      if (compare(item))
+        filteredItems.add(item);
+    }
+  }
+
+  protected void notifyFilterChanged() {
+    filter();
+
+    if (onFilterChanged != null)
+      onFilterChanged.exec(filteredItems);
+  }
+
+  public List<T> getFilteredItems() {
+    return filteredItems;
+  }
+}
